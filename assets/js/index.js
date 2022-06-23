ceccd4e7d65b86bbbde54d95869238d97eb5780b
+ <+>
2022-06-23 21:02:21 +0300

    added new event listener

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,8 @@ const button = document.querySelector('.container button');
 const jokeText = document.querySelector('.container p');
 document.addEventListener('DOMContentLoaded', getJoke)
 
+jokeText.addEventListener('mouseover',getJoke)
+
 button.addEventListener('click', getJoke);
 async function getJoke(){
     const jokeData = await fetch('https://icanhazdadjoke.com/', {
